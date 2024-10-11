8345675c7dc1c3d25e80de5f3e5320cde4617ca4
terry-for-github <anzi20000525@163.com>
2024-10-11 17:48:48 +0000

    update output_dir('-ov') and save_steps(5k)

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -15,14 +15,14 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./ckpts/llava-v1.5-7b \
+    --output_dir ./ckpts/llava-v1.5-7b-ov \
     --num_train_epochs 1 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 50000 \
+    --save_steps 5000 \
     --save_total_limit 1 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
