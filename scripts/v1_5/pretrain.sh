905edebb0a947403cb75350489e76e8bec74ea90
terry-for-github <anzi20000525@163.com>
2024-10-14 18:33:55 +0000

    llava for qwen2.5

--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -2,7 +2,7 @@
 
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero0.json \
-    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --model_name_or_path Qwen/Qwen2.5-3B-Instruct \
     --version plain \
     --data_path ./playground/pretrain/blip_laion_cc_sbu_558k.json \
     --image_folder ./playground/pretrain/images \
@@ -13,12 +13,12 @@ deepspeed llava/train/train_mem.py \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir ./ckpts/llava-v1.5-7b-pretrain \
+    --output_dir ./ckpts/llava-qwen25-3b-pretrain \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 2 \
-    --evaluation_strategy "no" \
+    --eval_strategy "no" \
     --save_strategy "steps" \
     --save_steps 24000 \
     --save_total_limit 1 \
